4d351136958822912674f9d85b333584e994eb3d
Bhavyamb-123 <bhavyabala03@gmail.com>
2025-07-23 13:05:27 +0530

    login complete

---
+++ todotask/todoapp/static/js/home.js
@@ -0,0 +1,12 @@
+function login(){
+    document.getElementById("log").style.display="flex";
+}
+function closing(){
+    document.getElementById("log").style.display="";
+}
+function register(){
+    document.getElementById("signup").style.display="flex";
+}
+function rem(){
+    document.getElementById("signup").style.display="";
+}
