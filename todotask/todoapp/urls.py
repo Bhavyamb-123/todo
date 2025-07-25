61ae80600a8998097c1a3c22ad269da5b85c0ea0
Bhavyamb-123 <bhavyabala03@gmail.com>
2025-07-25 11:52:24 +0530

    profilecreationandlogincompleted

--- todotask/todoapp/urls.py
+++ todotask/todoapp/urls.py
@@ -4,4 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.homepage,name="homepage"),
     path('taskpage/',views.taskpage, name="taskpage"),
+    path('todoedit/',views.todoedit, name="todoedit"),
+    path('pagelogin/',views.pagelogin, name="pagelogin"),
+    path('logout', views.userlogout,name="logout"),
 ]
